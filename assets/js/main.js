d12eaae3da5fccf9f917b21e27d88f5ee3a2109e
Tasbir-Glitch <82874560+Tasbir-Glitch@users.noreply.github.com>
2021-12-26 03:45:03 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+(function ($) {
+  "user strict";
+
+// let logo = assets/images/logo.png;
+
+// if(localStorage.getItem('logo')){
+//   $('.logo').addClass('change');
+//   logo = assets/images/logo2.png;
+// }else{
+//   $('.logo').removeClass('change');
+//   logo = assets/images/logo1.png;
+// }
+
+// $('.change-combo').on('click', function () {
+//   $('.logo').addClass('change');
+//   localStorage.setItem('logo', true);
+//   logo = assets/images/logo2.png;
+//   setLogo();
+// });
+
+// $('.change-combo').on('click', function () {
+//   $('.logo').removeClass('change');
+//   localStorage.removeItem('logo');
+//   logo = assets/images/logo.png;
+//   setLogo();
+// })
+
+// function setLogo(){
+//     $('logo').attr('src', logo);
+//   }
+ 
+
+
+  var swiper = new Swiper(".mySwiper", {
+        effect: "cards",
+        autoplay: false,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.slider-next',
+            prevEl: '.slider-prev',
+          },
+          autoplay: {
+            speed: 500,
+            delay: 1000,
+          },
+          speed: 500,
+
+      });
+})(jQuery);
